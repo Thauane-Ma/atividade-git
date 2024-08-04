689083d81ac310aefd16dff2ac0badc0b28367f1
Luis Filipe <luis.limao@escolar.ifrn.edu.br>
2024-08-04 17:30:49 -0300

    criação do outro template

--- Atividade/urls.py
+++ Atividade/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path('',views.thauane),
+    path('luis',views.luis),
 ]
--- Atividade/views.py
+++ Atividade/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def thauane(request):
     return render(request, 'thauane.html')
+
+def luis(request):
+    return render(request, 'luis.html')
